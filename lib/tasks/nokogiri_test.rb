0e0e0b4cc015e0ae3d72a3421540c818a9ba8595
danid123 <danid123@users.noreply.github.com>
2014-06-20 23:58:52 -0700

    updating nokogiri_test

--- lib/tasks/nokogiri_test.rb
+++ lib/tasks/nokogiri_test.rb
@@ -4,9 +4,16 @@ require 'open-uri'
 
 url = "http://prod3.agileticketing.net/websales/pages/list.aspx?epguid=5e3ea987-8f29-408f-ad74-5c32388b1f83&"
 doc = Nokogiri::HTML(open(url))
-doc.css(".Item , .ViewLink").each do |event|
-  title = event.at_css(".Name").text
-  start_time = event.at_css(".Time , span").text
-  url = "http://prod3.agileticketing.net/websales/pages/"+event.at_css(".ViewLink")[:href]
-  puts "#{title} - #{start_time} - #{url}"
+doc.css(".DaysRow td").each do |day|
+	start_date = day.at_css(".Date").text
+	
+	day.css(".Item").each do |event|
+
+	  title = event.at_css(".Name").text
+	  start_time = event.at_css(".Time , span").text
+	  start_date = day.at_css(".Date").text
+	  url = "http://prod3.agileticketing.net/websales/pages/"+event.at_css(".ViewLink")[:href]
+	  puts "#{title} - #{start_date} - #{start_time} - #{url}"
+	end
+
 end
